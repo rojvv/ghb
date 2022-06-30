dbae694cdf9041b441720c6bc4cc81f7c16e54dd
Roj <ez@roj.im>
2022-06-30 13:19:37 +0300

    Use parse-mode 1.3.1

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+export { Bot } from "https://deno.land/x/grammy@v1.9.0/mod.ts";
+export * from "https://deno.land/x/grammy_parse_mode@1.3.1/mod.ts";
