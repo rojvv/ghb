5fbfa397b3f527501a312bdd452e46b44ece913c
Roj <ez@roj.im>
2022-07-29 22:39:35 +0300

    rewrite messages

--- messages.ts
+++ messages.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-explicit-any
 // Credits go to github.com/vrumger/gibhugbot.
-import { bold, fmt, FormattedString, italic, link } from "./deps.ts";
+import { fmt, FormattedString, italic, link } from "./deps.ts";
 
 export const messages: Record<
   string,
@@ -10,26 +10,27 @@ export const messages: Record<
     if (payload.commits.length <= 0) {
       return;
     }
-    return fmt`${
-      bold(
-        fmt`🔨 ${
-          link(
-            fmt`${payload.commits.length} new commit${
-              payload.commits.length == 1 ? "" : "s"
-            }`,
-            payload.compare,
-          )
-        } to ${payload.repository.name}:${
-          payload.ref.split("/")[2] ?? payload.ref
-        }`,
+    return fmt`${fmt`${
+      link(
+        fmt`@${payload.sender.login}`,
+        payload.sender.html_url,
       )
-    }\n\n${
+    } pushed ${
+      link(
+        fmt`${payload.commits.length} commit${
+          payload.commits.length == 1 ? "" : "s"
+        }`,
+        payload.compare,
+      )
+    } to ${payload.repository.name}:${
+      payload.ref.split("/")[2] ?? payload.ref
+    }:`}\n\n${
       fmt(
         [""].concat(payload.commits.map(() => "")),
         ...payload.commits.map((v: any) =>
           fmt`${link(v.id.slice(0, 7), v.url)}: ${
             v.message.split("\n").map((v: string) => v)[0]
-          } by ${v.author.name}\n`
+          }\n`
         ),
       )
     }`;
@@ -37,21 +38,17 @@ export const messages: Record<
   "issues": (payload) => {
     switch (payload.action) {
       case "opened": {
-        const header = fmt`${
-          bold(
-            fmt`🐛 New issue: ${
-              link(
-                fmt`${payload.repository.name}#${payload.issue.number} ${payload.issue.title}`,
-                payload.issue.html_url,
-              )
-            }`,
-          )
-        }\nby ${
+        const header = fmt`${fmt`${
           link(
             fmt`@${payload.issue.user.login}`,
             payload.issue.user.html_url,
           )
-        }\n\n`;
+        } opened ${
+          link(
+            fmt`${payload.repository.name}#${payload.issue.number} ${payload.issue.title}`,
+            payload.issue.html_url,
+          )
+        }`}\n\n`;
         return fmt(
           ["", "", ""],
           header,
@@ -65,21 +62,17 @@ export const messages: Record<
   "pull_request": (payload) => {
     switch (payload.action) {
       case "opened": {
-        const header = fmt`${
-          bold(
-            fmt`🔌 New pull request: ${
-              link(
-                fmt`${payload.repository.name}#${payload.pull_request.number} ${payload.pull_request.title}`,
-                payload.pull_request.html_url,
-              )
-            }`,
-          )
-        }\nby ${
+        const header = fmt`${fmt`${
           link(
             fmt`@${payload.pull_request.user.login}`,
             payload.pull_request.user.html_url,
           )
-        }\n\n`;
+        } opened ${
+          link(
+            fmt`${payload.repository.name}#${payload.pull_request.number} ${payload.pull_request.title}`,
+            payload.pull_request.html_url,
+          )
+        }.`}\n\n`;
         return fmt(
           ["", "", ""],
           header,
@@ -89,75 +82,67 @@ export const messages: Record<
         );
       }
       case "review_requested": {
-        return bold(
-          fmt`👁‍🗨 New review request on ${
-            link(
-              fmt`${payload.repository.name}#${payload.pull_request.number} ${payload.pull_request.title}`,
-              payload.pull_request.html_url,
-            )
-          } from ${
-            link(
-              fmt`@${payload.sender.login}`,
-              payload.sender.html_url,
-            )
-          } to ${
-            link(
-              fmt`@${payload.requested_reviewer.login}`,
-              payload.requested_reviewer.html_url,
-            )
-          }`,
-        );
+        return fmt`${
+          link(
+            fmt`@${payload.sender.login}`,
+            payload.sender.html_url,
+          )
+        } requested ${
+          link(
+            fmt`@${payload.requested_reviewer.login}`,
+            payload.requested_reviewer.html_url,
+          )
+        } to review ${
+          link(
+            fmt`${payload.repository.name}#${payload.pull_request.number} ${payload.pull_request.title}`,
+            payload.pull_request.html_url,
+          )
+        }.`;
       }
     }
   },
   "star": (payload) => {
     switch (payload.action) {
       case "created":
-        return bold(
-          fmt`💫 New star on ${
-            link(payload.repository.name, payload.repository.html_url)
-          } by ${
-            link(fmt`@${payload.sender.login}`, payload.sender.html_url)
-          } `,
-        );
+        return fmt`${
+          link(fmt`@${payload.sender.login}`, payload.sender.html_url)
+        } starred ${
+          link(payload.repository.name, payload.repository.html_url)
+        }.`;
     }
   },
   "fork": (payload) => {
     return fmt`${
-      bold(
-        fmt`🍴 New fork: ${
-          link(payload.forkee.full_name, payload.forkee.html_url)
-        }`,
-      )
-    }\nby ${link(fmt`@${payload.sender.login}`, payload.sender.html_url)}`;
+      link(fmt`@${payload.sender.login}`, payload.sender.html_url)
+    } forked ${link(payload.forkee.full_name, payload.forkee.html_url)} from ${
+      link(payload.repository.full_name, payload.repository.html_url)
+    }.`;
   },
   "pull_request_review": (payload) => {
     switch (payload.action) {
       case "submitted": {
-        const emoji = ({
-          "commented": "⚪️",
-          "approved": "🟢",
-          "changes_requested": "🟡",
+        const verb = ({
+          "commented": "reviewed",
+          "approved": "approved",
+          "changes_requested": "requested changes on",
         } as Record<string, string>)[payload.review.state];
-        if (!emoji) {
+        if (!verb) {
           return;
         }
         const header = fmt`${
-          bold(fmt`${
-            link(
-              fmt`${emoji} New review: ${payload.repository.name}#${payload.pull_request.number} ${payload.pull_request.title} ${payload.review.id}`,
-              payload.review.html_url,
-            )
-          }`)
-        }\nby ${
           link(fmt`@${payload.sender.login}`, payload.sender.html_url)
+        } ${verb} ${
+          link(
+            fmt`${payload.repository.name}#${payload.pull_request.number} ${payload.pull_request.title}`,
+            payload.pull_request.html_url,
+          )
         }\n\n`;
         return fmt(
           ["", "", ""],
           header,
           payload.review.body
             ? payload.review.body.slice(0, 4096 - header.text.length)
-            : italic("No description provided."),
+            : "",
         );
       }
     }
@@ -166,16 +151,14 @@ export const messages: Record<
     switch (payload.action) {
       case "created": {
         const header = fmt`${
-          bold(
-            link(
-              fmt`💬 New comment: ${payload.repository.name}#${payload.issue.number} ${payload.issue.title} ${payload.comment.id}`,
-              payload.comment.html_url,
-            ),
-          )
-        }\nby ${
           link(
             fmt`@${payload.comment.user.login}`,
             payload.comment.user.html_url,
+          )
+        } ${
+          link(
+            fmt`commented on ${payload.repository.name}#${payload.issue.number} ${payload.issue.title} ${payload.comment.id}`,
+            payload.comment.html_url,
           )
         }\n\n`;
         return fmt(
