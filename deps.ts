58775e151e4e7f79cdf6e37ac6f6c53c83d83aa1
Roj <ez@roj.im>
2022-12-02 15:33:37 +0300

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export { Bot } from "https://deno.land/x/grammy@v1.11.0/mod.ts";
-export * from "https://deno.land/x/grammy_parse_mode@1.4.0/mod.ts";
+export { Bot } from "https://deno.land/x/grammy@v1.11.2/mod.ts";
+export * from "https://deno.land/x/grammy_parse_mode@1.5.0/mod.ts";
